d2730ae3e41e797a548cf53de50d7f0adcbdfc0d
WhybidTeodoro <twhybid@gmail.com>
2025-12-29 18:02:23 -0300

    Criado entidade Player, representa os jogadores do time do usuario

---
+++ FootStatsAPI/Models/Player.cs
@@ -0,0 +1,17 @@
+﻿
+using FootStatsAPI.Models;
+
+public class Player
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Position { get; set; } = string.Empty;
+    public int ShirtNumber { get; set; }
+    public int Goals { get; set; }
+    public int Assist { get; set; }
+    public int MatchesPlayed { get; set; }
+    public int TeamId { get; set; }
+    public Team Team { get; set; } = null!;
+
+
+}
