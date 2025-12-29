50802f163e60957d6e401078770c0a0a778362a8
WhybidTeodoro <twhybid@gmail.com>
2025-12-29 17:49:43 -0300

    Criado entidade Team, representa o time do usuario

---
+++ FootStatsAPI/Models/Team.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace FootStatsAPI.Models;
+
+public class Team
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public int UserId { get; set; }
+    public User User { get; set; } = null!;
+    public ICollection<Player> Players { get; set; } = new List<Player>();
+    public ICollection<Match> Matches { get; set; } = new List<Match>();
+
+
+}
