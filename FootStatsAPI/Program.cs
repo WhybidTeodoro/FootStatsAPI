84e2c57592463b08a197b504825d97012bb5b14f
WhybidTeodoro <twhybid@gmail.com>
2025-12-29 14:36:03 -0300

    Configuração JWT Bearer no appsettings e program.cs para validações da API

--- FootStatsAPI/Program.cs
+++ FootStatsAPI/Program.cs
@@ -1,5 +1,9 @@
 using FootStatsAPI.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,11 +20,80 @@ builder.Services.AddDbContext<FootDbContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
 });
 
+// ===================
+//  Configuração JWT
+// ===================
+
+//Lê a sessão do jwt do app settings
+var jwtSettings = builder.Configuration.GetSection("Jwt");
+
+//Obtem a chave secreta e converte para bytes
+var key = Encoding.ASCII.GetBytes(jwtSettings["Key"]!);
+
+//Registra o servico de autenticação
+builder.Services.AddAuthentication(options =>
+{
+    //Define que o padrão de autenticação sera JwtBeare
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        //valida se o token foi assinado da forma correta
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+
+        //valida quem emitiu o token
+        ValidateIssuer = true,
+        ValidIssuer = jwtSettings["Issuer"],
+
+        //valida quem vai receber o token
+        ValidateAudience = true,
+        ValidAudience = jwtSettings["Audience"],
+
+        //valida se o token expirou
+        ValidateLifetime = true,
+
+        //Remove tolerancia de tempo extra
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    //Define o esquema de segurança JWT
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Informe o token jwt dessa forma: Bearer seu_token"
+    });
+
+    //Exige o token para endpoints protegidos
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 var app = builder.Build();
 
@@ -32,7 +105,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
