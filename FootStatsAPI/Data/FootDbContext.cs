dbb2fd6e6e66e89bb6551e7d057e06ff150f208b
WhybidTeodoro <twhybid@gmail.com>
2025-12-29 14:12:48 -0300

    Configuração inicial do DbContext

---
+++ FootStatsAPI/Data/FootDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FootStatsAPI.Data;
+
+public class FootDbContext : DbContext
+{
+    public FootDbContext(DbContextOptions<FootDbContext> options) : base(options) { }
+}
--- FootStatsAPI/Program.cs
+++ FootStatsAPI/Program.cs
@@ -1,6 +1,21 @@
+using FootStatsAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// ====================
+//   Config DbContext
+// ====================
+
+builder.Services.AddDbContext<FootDbContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("MySqlConnection");
+
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
