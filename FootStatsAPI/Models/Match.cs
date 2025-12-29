29d6c6c78e6b686928cf8c9c0f443e09a0caf2ce
WhybidTeodoro <twhybid@gmail.com>
2025-12-29 18:05:57 -0300

    Criado entidade Match, representa as partidas do time

---
+++ FootStatsAPI/Models/Match.cs
@@ -0,0 +1,12 @@
+﻿namespace FootStatsAPI.Models;
+
+public class Match
+{
+    public int Id { get; set; }
+    public DateTime MatchData { get; set; }
+    public string OpponentTeam { get; set; } = string.Empty;
+    public int GoalsFor { get; set; }
+    public int GoalsAgainst { get; set; }
+    public int TeamId { get; set; }
+    public Team Team { get; set; } = null!;
+}
