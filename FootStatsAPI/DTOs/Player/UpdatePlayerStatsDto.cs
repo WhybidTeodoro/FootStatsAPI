4c2da540580a1acf39ab49d6653121776fd47cf8
WhybidTeodoro <twhybid@gmail.com>
2025-12-30 17:09:50 -0300

    Criado Dtos para Player

---
+++ FootStatsAPI/DTOs/Player/UpdatePlayerStatsDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FootStatsAPI.DTOs.Player;
+
+public class UpdatePlayerStatsDto
+{
+    // <summary>
+    /// Quantidade de gols do jogador
+    /// </summary>
+    [Range(0, int.MaxValue, ErrorMessage = "O Numero de gols não pode ser negativo")]
+    public int Goals { get; set; }
+
+    /// <summary>
+    /// Quantidade de assistencias do jogador
+    /// </summary>
+    [Range(0, int.MaxValue, ErrorMessage = "O Numero de assistencias não pode ser negativo")]
+    public int Assists { get; set; }
+
+    /// <summary>
+    /// Quantidade de Partidas jogadas pelo jogador
+    /// </summary>
+    [Range(0, int.MaxValue, ErrorMessage = "O Numero de jogos não pode ser negativo")]
+    public int MatchesPlayed { get; set; }
+}
--- FootStatsAPI/Program.cs
+++ FootStatsAPI/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using System.Reflection;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
