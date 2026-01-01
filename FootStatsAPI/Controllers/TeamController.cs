66c6a6bce066b428cd42badf6ef0ca58dbb98615
WhybidTeodoro <twhybid@gmail.com>
2026-01-01 10:52:10 -0300

    Adicionado DTOs para Match

--- FootStatsAPI/Controllers/TeamController.cs
+++ FootStatsAPI/Controllers/TeamController.cs
@@ -3,7 +3,6 @@ using FootStatsAPI.DTOs.Player;
 using FootStatsAPI.DTOs.Team;
 using FootStatsAPI.Models;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
---
+++ FootStatsAPI/DTOs/Match/MatchResponseDto.cs
@@ -0,0 +1,32 @@
+﻿namespace FootStatsAPI.DTOs.Match;
+
+/// <summary>
+/// Dto utilizado para retorno de dados da partida
+/// </summary>
+public class MatchResponseDto
+{
+    /// <summary>
+    /// Identifador unico da partida
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Data da partida.
+    /// </summary>
+    public DateTime MatchDate { get; set; }
+
+    /// <summary>
+    /// Nome do time adversario.
+    /// </summary>
+    public string OpponentTeam { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gols a favor na partida.
+    /// </summary>
+    public int GoalsFor { get; set; }
+
+    /// <summary>
+    /// Gols do adversario na partida.
+    /// </summary>
+    public int GoalsAgainst { get; set; }
+}
