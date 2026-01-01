f9458833b547952dafb0200498e9e07e5afe5061
WhybidTeodoro <twhybid@gmail.com>
2026-01-01 15:20:09 -0300

    Criado StatsController, responsavel pelas estatisticas dos times do usuario

---
+++ FootStatsAPI/Controllers/StatsController.cs
@@ -0,0 +1,21 @@
+﻿using FootStatsAPI.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FootStatsAPI.Controllers;
+
+/// <summary>
+/// Controller responsavel por calcular a estatisticas dos times
+/// </summary>
+[Route("api/[controller]")]
+[ApiController]
+[Authorize]
+public class StatsController : ControllerBase
+{
+    private readonly FootDbContext _context;
+
+    public StatsController(FootDbContext context)
+    {
+        _context = context;
+    }
+}
