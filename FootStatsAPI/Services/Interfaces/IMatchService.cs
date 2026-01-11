13c38a743c9d13268865d80c25c2b78fee61e22e
WhybidTeodoro <twhybid@gmail.com>
2026-01-11 11:15:52 -0300

    Criado interface IMatchService

---
+++ FootStatsAPI/Services/Interfaces/IMatchService.cs
@@ -0,0 +1,14 @@
+﻿using FootStatsAPI.DTOs.Match;
+
+namespace FootStatsAPI.Services.Interfaces;
+
+public interface IMatchService
+{
+    public Task<MatchResponseDto> AddMatchAsync(int userId, CreateMatchDto dto);
+
+    public Task<MatchResponseDto> GetByIdAsync(int userId, int id);
+
+    public Task<MatchResponseDto> UpdateMatchAsync(int userId, int id, UpdateMatchDto dto);
+
+    public Task DeleteMatchAsync(int userId, int id);
+}
