cef826469d4771255c371a4cba485693c9029ab9
WhybidTeodoro <twhybid@gmail.com>
2026-01-11 11:17:34 -0300

    Criado MatchService

---
+++ FootStatsAPI/Services/MatchService.cs
@@ -0,0 +1,27 @@
+﻿using FootStatsAPI.DTOs.Match;
+using FootStatsAPI.Services.Interfaces;
+
+namespace FootStatsAPI.Services;
+
+public class MatchService : IMatchService
+{
+    public Task<MatchResponseDto> AddMatchAsync(int userId, CreateMatchDto dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<MatchResponseDto> GetByIdAsync(int userId, int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<MatchResponseDto> UpdateMatchAsync(int userId, int id, UpdateMatchDto dto)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public Task DeleteMatchAsync(int userId, int id)
+    {
+        throw new NotImplementedException();
+    }
+}
