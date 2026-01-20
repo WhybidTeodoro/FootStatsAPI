07a8427ea1b9604f375ac6dd8eb7c81af4f6e8a9
WhybidTeodoro <twhybid@gmail.com>
2026-01-19 22:15:20 -0300

    Criado Interface IPlayerRepository e classe PlayerRepository

---
+++ FootStats.Application/Services/Interfaces/Repositories/IPlayerRepository.cs
@@ -0,0 +1,13 @@
+﻿using FootStatsAPI.Models;
+
+namespace FootStats.Application.Services.Interfaces.Repositories;
+
+public interface IPlayerRepository
+{
+    Task AddAsync(Player player);
+    Task<List<Player>> GetAllByTeamAsync(int userId, int teamId);
+    Task<Player?> GetByIdAsync(int userId, int id);
+    Task UpdateAsync(Player player);
+    Task DeleteAsync(Player player);
+    Task SaveChangesAsync();
+}
---
+++ FootStats.Infrastructure/Repositories/PlayerRepository.cs
@@ -0,0 +1,38 @@
+﻿using FootStats.Application.Services.Interfaces.Repositories;
+using FootStatsAPI.Models;
+
+namespace FootStats.Infrastructure.Repositories
+{
+    internal class PlayerRepository : IPlayerRepository
+    {
+        public Task AddAsync(Player player)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(Player player)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Player>> GetAllByTeamAsync(int userId, int teamId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Player?> GetByIdAsync(int userId, int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Player player)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
