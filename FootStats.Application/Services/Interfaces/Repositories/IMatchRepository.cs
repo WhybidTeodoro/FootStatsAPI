6d70bb1bd1fa11f0e44c8efc7709156560ca179f
WhybidTeodoro <twhybid@gmail.com>
2026-01-22 19:31:01 -0300

    Criado interface IMatchRepository

---
+++ FootStats.Application/Services/Interfaces/Repositories/IMatchRepository.cs
@@ -0,0 +1,12 @@
+﻿using FootStatsAPI.Models;
+
+namespace FootStats.Application.Services.Interfaces.Repositories;
+
+public interface IMatchRepository
+{
+    Task<Match> AddAsync(Match match);
+    Task<Match?> GetByIdAsync(int userId, int id);
+    Task<List<Match>> GetAllMatchesByTeamAsync(int userId, int teamId);
+    Task UpdateAsync(Match match);
+    Task DeleteAsync(Match match);
+}
