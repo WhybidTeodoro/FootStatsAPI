564ed356e9725460fa37f08fb5b066f43f1ca7b3
WhybidTeodoro <twhybid@gmail.com>
2026-01-22 19:54:22 -0300

    Criado a classe MatchRepository

--- FootStats.Application/Services/Interfaces/Repositories/IMatchRepository.cs
+++ FootStats.Application/Services/Interfaces/Repositories/IMatchRepository.cs
@@ -4,7 +4,7 @@ namespace FootStats.Application.Services.Interfaces.Repositories;
 
 public interface IMatchRepository
 {
-    Task<Match> AddAsync(Match match);
+    Task AddAsync(Match match);
     Task<Match?> GetByIdAsync(int userId, int id);
     Task<List<Match>> GetAllMatchesByTeamAsync(int userId, int teamId);
     Task UpdateAsync(Match match);
---
+++ FootStats.Infrastructure/Repositories/MatchRepository.cs
@@ -0,0 +1,43 @@
+﻿using FootStats.Application.Services.Interfaces.Repositories;
+using FootStats.Infrastructure.Data;
+using FootStatsAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FootStats.Infrastructure.Repositories;
+
+public class MatchRepository : IMatchRepository
+{
+    private readonly FootDbContext _context;
+
+    public MatchRepository(FootDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddAsync(Match match)
+    {
+       await _context.Matches.AddAsync(match);
+    }
+
+    public Task DeleteAsync(Match match)
+    {
+        _context.Matches.Remove(match);
+        return Task.CompletedTask;
+    }
+
+    public async Task<List<Match>> GetAllMatchesByTeamAsync(int userId, int teamId)
+    {
+        return await _context.Matches.Where(m => m.Team.UserId == userId && m.TeamId == teamId).ToListAsync();
+    }
+
+    public async Task<Match?> GetByIdAsync(int userId, int id)
+    {
+        return await _context.Matches.FirstOrDefaultAsync(m => m.Team.UserId == userId && m.Id == id);
+    }
+
+    public Task UpdateAsync(Match match)
+    {
+        _context.Matches.Update(match);
+        return Task.CompletedTask;
+    }
+}
