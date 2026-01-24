f4b9a24c60181050c5c3458d6382cc089e93cfac
WhybidTeodoro <twhybid@gmail.com>
2026-01-24 12:47:12 -0300

    Criado validator para UpdatePlayerStatsDtoValidator

--- FootStats.Application/DTOs/Player/UpdatePlayerStatsDto.cs
+++ FootStats.Application/DTOs/Player/UpdatePlayerStatsDto.cs
@@ -7,18 +7,15 @@ public class UpdatePlayerStatsDto
     // <summary>
     /// Quantidade de gols do jogador
     /// </summary>
-    [Range(0, int.MaxValue, ErrorMessage = "O Numero de gols não pode ser negativo")]
     public int Goals { get; set; }
 
     /// <summary>
     /// Quantidade de assistencias do jogador
     /// </summary>
-    [Range(0, int.MaxValue, ErrorMessage = "O Numero de assistencias não pode ser negativo")]
     public int Assists { get; set; }
 
     /// <summary>
     /// Quantidade de Partidas jogadas pelo jogador
     /// </summary>
-    [Range(0, int.MaxValue, ErrorMessage = "O Numero de jogos não pode ser negativo")]
     public int MatchesPlayed { get; set; }
 }
