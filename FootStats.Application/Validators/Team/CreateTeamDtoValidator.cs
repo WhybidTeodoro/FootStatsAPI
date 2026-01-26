0b3be4d8d92133a2c2ca84a03e2fab76223fdc6f
WhybidTeodoro <twhybid@gmail.com>
2026-01-26 20:38:36 -0300

    Criado validators para dto's create e update de team

---
+++ FootStats.Application/Validators/Team/CreateTeamDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using FootStatsAPI.DTOs.Team;
+
+namespace FootStats.Application.Validators.Team;
+
+/// <summary>
+/// Responsavel pela validação dos dados na criação do time
+/// </summary>
+public class CreateTeamDtoValidator : AbstractValidator<CreateTeamDto>
+{
+    public CreateTeamDtoValidator()
+    {
+        RuleFor(t => t.Name)
+            .NotEmpty().WithMessage("O Nome do time é obrigatório")
+            .MaximumLength(500).WithMessage("O Nome do time deve ter no máximo 50 caracteres");
+    }
+}
---
+++ FootStats.Application/Validators/Team/UpdateTeamDtoValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using FootStatsAPI.DTOs.Team;
+
+namespace FootStats.Application.Validators.Team;
+
+public class UpdateTeamDtoValidator : AbstractValidator<UpdateTeamDto>
+{
+	public UpdateTeamDtoValidator()
+	{
+        RuleFor(t => t.Name)
+            .NotEmpty().WithMessage("O Nome do time é obrigatório")
+            .MaximumLength(500).WithMessage("O Nome do time deve ter no máximo 50 caracteres");
+    }
+}
