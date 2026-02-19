72d609b95cdefc7a8d4362ceefab1dd8b4ba373a
WhybidTeodoro <twhybid@gmail.com>
2026-02-19 17:39:03 -0300

    feat: Adicionado Validator PaginationParametersValidator

---
+++ FootStats.Application/Validators/Pagination/PaginationParametersValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using FootStats.Application.Common.Pagination;
+
+namespace FootStats.Application.Validators.Pagination;
+
+/// <summary>
+/// Validação dos parâmetros de paginação.
+/// </summary>
+public class PaginationParametersValidator
+    : AbstractValidator<PaginationParameters>
+{
+    public PaginationParametersValidator()
+    {
+        RuleFor(x => x.PageNumber)
+            .GreaterThanOrEqualTo(PaginationParameters.MinPageNumber)
+            .WithMessage("PageNumber deve ser maior ou igual a 1.");
+
+        RuleFor(x => x.PageSize)
+            .GreaterThanOrEqualTo(PaginationParameters.MinPageSize)
+            .WithMessage("PageSize deve ser maior ou igual a 1.")
+            .LessThanOrEqualTo(PaginationParameters.MaxPageSize)
+            .WithMessage($"PageSize deve ser no máximo {PaginationParameters.MaxPageSize}.");
+    }
+}
