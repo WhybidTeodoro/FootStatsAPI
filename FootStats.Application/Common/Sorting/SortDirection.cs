6d6275f66052330763947d249ab52436b30044c7
WhybidTeodoro <twhybid@gmail.com>
2026-02-23 21:20:02 -0300

    adicionar parametros sorting query e contrato base

---
+++ FootStats.Application/Common/Sorting/SortDirection.cs
@@ -0,0 +1,11 @@
+﻿namespace FootStats.Application.Common.Sorting
+{
+    /// <summary>
+    /// Direção da ordenação.
+    /// </summary>
+    public enum SortDirection
+    {
+        Asc = 0,
+        Desc = 1
+    }
+}
