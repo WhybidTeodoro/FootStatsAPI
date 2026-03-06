cfc579caabdae88beba4beeab8a2496a63d49c11
WhybidTeodoro <twhybid@gmail.com>
2026-03-06 13:34:48 -0300

    test: adicionar testes para sorting, com parser de direção e fallback

---
+++ FootStats.Tests/Common/Sorting/SortParametersParserTests.cs
@@ -0,0 +1,90 @@
+﻿using FluentAssertions;
+using FootStats.Application.Common.Sorting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootStats.Tests.Common.Sorting;
+
+public class SortParametersParserTests
+{
+    [Fact]
+    public void ParseOrDefault_DeveRetornarAsc_QuandoSortDirectionForNull()
+    {
+        // Arrange
+        string? sortDirection = null;
+
+        // Act
+        var result = SortParametersParser.ParseOrDefault(sortDirection);
+
+        // Assert
+        result.Should().Be(SortDirection.Asc);
+    }
+
+    [Fact]
+    public void ParseOrDefault_DeveRetornarAsc_QuandoSortDirectionForVazio()
+    {
+        // Arrange
+        var sortDirection = string.Empty;
+
+        // Act
+        var result = SortParametersParser.ParseOrDefault(sortDirection);
+
+        // Assert
+        result.Should().Be(SortDirection.Asc);
+    }
+
+    [Fact]
+    public void ParseOrDefault_DeveRetornarAsc_QuandoSortDirectionForAsc()
+    {
+        // Arrange
+        var sortDirection = "asc";
+
+        // Act
+        var result = SortParametersParser.ParseOrDefault(sortDirection);
+
+        // Assert
+        result.Should().Be(SortDirection.Asc);
+    }
+
+    [Fact]
+    public void ParseOrDefault_DeveRetornarDesc_QuandoSortDirectionForDesc()
+    {
+        // Arrange
+        var sortDirection = "desc";
+
+        // Act
+        var result = SortParametersParser.ParseOrDefault(sortDirection);
+
+        // Assert
+        result.Should().Be(SortDirection.Desc);
+    }
+
+    [Fact]
+    public void ParseOrDefault_DeveRetornarDesc_QuandoSortDirectionForDescEmMaiusculo()
+    {
+        // Arrange
+        var sortDirection = "DESC";
+
+        // Act
+        var result = SortParametersParser.ParseOrDefault(sortDirection);
+
+        // Assert
+        result.Should().Be(SortDirection.Desc);
+    }
+
+    [Fact]
+    public void ParseOrDefault_DeveRetornarAsc_QuandoSortDirectionForInvalido()
+    {
+        // Arrange
+        var sortDirection = "qualquercoisa";
+
+        // Act
+        var result = SortParametersParser.ParseOrDefault(sortDirection);
+
+        // Assert
+        result.Should().Be(SortDirection.Asc);
+    }
+}
