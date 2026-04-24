6a461b5aa4ce699423e76fc83f58d3af8a3285e9
WhybidTeodoro <twhybid@gmail.com>
2026-04-24 20:04:16 -0300

    feat(frontend): manipula tabela via javascript com innerHTML

--- FrontEnd/scripts/main.js
+++ FrontEnd/scripts/main.js
@@ -1,1 +1,10 @@
-console.log("JS carregado com sucesso");
+const teamsTable = document.getElementById("teamsTable");
+
+if (teamsTable) {
+    teamsTable.innerHTML = `
+        <tr>
+            <td>99</td>
+            <td>Time Teste JS</td>
+        </tr>
+    `;
+}
