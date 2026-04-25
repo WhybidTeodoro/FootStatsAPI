a040229698f200162041d83e24a33c8676bab6ea
WhybidTeodoro <twhybid@gmail.com>
2026-04-25 16:49:37 -0300

    feat(frontend): integra listagem de times com api .net via fetch

--- FrontEnd/scripts/main.js
+++ FrontEnd/scripts/main.js
@@ -1,14 +1,52 @@
-function loadTeams() {
-    const teamsTable = document.getElementById("teamsTable");
+const API_BASE_URL = "https://localhost:7198/api";
 
-    teamsTable.innerHTML = `
-        <tr>
-            <td>1</td>
-            <td>Carregado via função</td>
-        </tr>
-    `;
+
+async function getTeams() {
+    try {
+        const response = await fetch(`${API_BASE_URL}/team`);
+
+        if (!response.ok) {
+            throw new Error("Erro ao buscar times na API");
+        }
+
+        const data = await response.json();
+
+        console.log("Resposta da API:", data);
+
+        // 👇 IMPORTANTE
+        renderTeams(data.items);
+
+    } catch (error) {
+        console.error("Erro na requisição:", error);
+    }
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-    loadTeams();
-});
+function renderTeams(teams){
+
+    const table = document.getElementById("teamsTable");
+
+    if(!table) return;
+
+    table.innerHTML = "";
+
+    if (!Array.isArray(teams)) {
+        console.error("teams não é um array:", teams);
+        return;
+    }
+
+    let rows = "";
+
+    teams.forEach(team => {
+        rows += `
+            <tr>
+                <td>${team.id}</td>
+                <td>${team.name}</td>
+            </tr>`;
+    });
+
+    table.innerHTML = rows;
+}
+
+document.addEventListener("DOMContentLoaded", () =>{
+    getTeams();
+})
